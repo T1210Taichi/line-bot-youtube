a0adaa331a73cec3d9c385d9ee187dd9584cc533
T1210Taichi <T1210Taichi@gmail.com>
2021-08-20 04:39:36 +0900

    20210820

--- app.py
+++ app.py
@@ -24,22 +24,35 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 def hello_world():
     return "hello world!"
 
+#1
+#Webhookからのリクエストをチェックする
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # リクエストヘッダーから署名検証のための値を取得する
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    #リクエストボディを取得する
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
+    #署名を検証し、問題なければhandleに定義されている関数を呼び出す
     try:
         handler.handle(body, signature)
+    #署名検証を失敗した場合、例外を出す
     except InvalidSignatureError:
         abort(400)
-
+    #handleの処理を終えればOK
     return 'OK'
+
+
+#2
+#LINEのメッセージの取得と返信無いようの定義
+
+#LINEでMEssageEvent(普通のメッセージを送信された場合)が起こったときに
+#def以下の関数を実行する
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
 
 #@handler.add(MessageEvent, message=TextMessage)
 #def handle_message(event):
@@ -60,5 +73,5 @@ def message_text(event):
 
 if __name__ == "__main__":
 #    app.run()
-    port = int(os.getenv("PORT"))
+    port = int(os.getenv("PORT",5000))
     app.run(host="0.0.0.0", port=port)
