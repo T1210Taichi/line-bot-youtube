758b1afb51c44832a46265d8a50309e0de3326a9
T1210Taichi <T1210Taichi@gmail.com>
2021-08-24 01:35:32 +0900

    20210824 send irasutoya img

--- main.py
+++ main.py
@@ -74,15 +74,13 @@ def callback():
 #        original_content_url="https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png",
 #        preview_image_url="https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png",
 #    )
-    
-    line_bot_api.reply_message(event.reply_token,img_irasutoya)
+#    
+#    line_bot_api.reply_message(event.reply_token,img_irasutoya)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     　line_bot_api.reply_message(
-    　　event.reply_token,
-    　　ImageSendMessage(
-    　　original_content_url=’https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png’,
+    　　event.reply_token,ImageSendMessage(original_content_url=’https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png’,
     　　preview_image_url=’https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png’))
 
 if __name__ == "__main__":
